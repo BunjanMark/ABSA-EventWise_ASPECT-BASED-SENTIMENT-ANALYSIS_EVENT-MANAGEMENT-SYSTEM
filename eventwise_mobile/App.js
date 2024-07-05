4b241e90c687b6002a00bed9bc10863158a5a815
adelyneyana <adelyneyana9@gmail.com>
2024-07-05 23:23:14 +0800

    Initial commit

--- eventwise_mobile/App.js
+++ eventwise_mobile/App.js
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+import register from "./components/authentication/register";
+import login from "./components/authentication/login";
+import AccountRecovery from "./components/authentication/AccountRecovery";
+import landing from "./components/authentication/landing";
+
+import GuestLanding from "./components/authentication/GuestLanding";
+
+import Screen from "./components/screens/Screen";
+import BookEvent from "./components/screens/BookEvent";  
+import EventDetails from "./components/screens/EventDetails";
+import InboxView from "./components/screens/InboxView";
+import ConversationView from "./components/screens/ConversationView";
+import SelectContactView from "./components/screens/SelectContactView";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="landing"
+            component={landing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="GuestLanding"
+            component={GuestLanding}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="login"
+            component={login}
+            options={{ headerShown: false }}
+          />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+          <Stack.Screen
+            name="register"
+            component={register}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="AccountRecovery"
+            component={AccountRecovery}
+            options={{ headerShown: false }}
+          />
+          {/* <Stack.Screen
+            name="Screen"
+            component={Screen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="BookEvent"
+            component={BookEvent}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="EventDetails"
+            component={EventDetails}
+            options={{ headerShown: false }}
+          />
+           <Stack.Screen
+            name="InboxView"
+            component={InboxView}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="ConversationView"
+            component={ConversationView}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="SelectContactView"
+            component={SelectContactView}
+            options={{ headerShown: false }}
+          /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+};
+
+export default App;
