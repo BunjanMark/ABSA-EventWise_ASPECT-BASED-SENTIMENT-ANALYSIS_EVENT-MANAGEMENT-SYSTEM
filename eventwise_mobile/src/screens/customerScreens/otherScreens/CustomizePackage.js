30056006927fc09f9832ac94d8af21afe0e30611
Bunjan, Mark <108747105+BunjanMark@users.noreply.github.com>
2024-12-15 10:49:04 +0800

    state where APK is applicable

--- eventwise_mobile/src/screens/customerScreens/otherScreens/CustomizePackage.js
+++ eventwise_mobile/src/screens/customerScreens/otherScreens/CustomizePackage.js
@@ -1,49 +1,25 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image, ImageBackground } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
-import SearchBar from '../elements/SearchBAr';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Image,
+  ImageBackground,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from "@react-navigation/native";
+import SearchBar from "../elements/SearchBAr";
 
 const CustomizePackage = () => {
   const navigation = useNavigation();
   const [showSearch, setShowSearch] = useState(false);
-  const [selectedTab, setSelectedTab] = useState('Photographer');
+  const [selectedTab, setSelectedTab] = useState("Photographer");
   const [selectedServices, setSelectedServices] = useState([]);
 
-  const serviceData = {
-    Photographer: [
-      { name: 'Diwata Photography', price: 10, image: require('../pictures/cp2.png') },
-      { name: '1 Photography', price: 10, image: require('../pictures/cp1.png') },
-      { name: '2 Photography', price: 10, image: require('../pictures/cp3.png') },
-    ],
-    'Food Catering': [
-      { name: 'Diwata Pares', price: 20, image: require('../pictures/cp1.png') },
-      { name: 'Pitik ni Diwata', price: 20, image: require('../pictures/cp.png') },
-      { name: 'Diwata ni', price: 20, image: require('../pictures/cp2.png') },
-    ],
-    Decoration: [
-      { name: 'Diwata Decor', price: 25, image: require('../pictures/cp3.png') },
-      { name: 'Diwata 1', price: 25, image: require('../pictures/cp2.png') },
-      { name: 'Diwata 2', price: 25, image: require('../pictures/cp1.png') },
-    ],
-    'Service A': [
-      { name: 'Service 1', price: 25, image: require('../pictures/cp3.png') },
-      { name: 'Service 2', price: 25, image: require('../pictures/cp.png') },
-      { name: 'Service 3', price: 25, image: require('../pictures/cp1.png') },
-    ],
-    'Service B': [
-      { name: 'Service 4', price: 25, image: require('../pictures/cp2.png') },
-      { name: 'Service 5', price: 25, image: require('../pictures/cp1.png') },
-      { name: 'Service 6', price: 25, image: require('../pictures/cp3.png') },
-    ],
-    'Service C': [
-      { name: 'Service 7', price: 25, image: require('../pictures/cp.png') },
-      { name: 'Service 8', price: 25, image: require('../pictures/cp2.png') },
-      { name: 'Service 9', price: 25, image: require('../pictures/cp1.png') },
-    ],
-  };
-
+  const serviceData = [];
   const handleSearch = (query) => {
     console.log("Searching for:", query);
     // Add your search logic here
@@ -58,33 +34,45 @@ const CustomizePackage = () => {
   };
 
   const handleSubmitPress = () => {
-    const total = selectedServices.reduce((sum, service) => sum + service.price, 0);
-    navigation.navigate('BookingContinuation2', { selectedPackage: selectedServices, totalPrice: total });
+    const total = selectedServices.reduce(
+      (sum, service) => sum + service.price,
+      0
+    );
+    navigation.navigate("BookingContinuation2", {
+      selectedPackage: selectedServices,
+      totalPrice: total,
+    });
   };
-  
-  const total = selectedServices.reduce((sum, service) => sum + service.price, 0);
+
+  const total = selectedServices.reduce(
+    (sum, service) => sum + service.price,
+    0
+  );
 
   return (
-    <View style={{ flex: 1, backgroundColor: "#fff" }}> 
-        <View style={styles.container}>
-          <SafeAreaView>
-            <View style={styles.head}>
-              <TouchableOpacity style={styles.closeButton} onPress={() => navigation.goBack()}>
-                <Icon name="close" size={24} color="#000" />
-              </TouchableOpacity>
-              <Image
-                source={require("../../../../assets/logoWhite.png")}
-                style={styles.logo}
-                resizeMode="contain"
-              />
-            </View>
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <SafeAreaView>
+          <View style={styles.head}>
+            <TouchableOpacity
+              style={styles.closeButton}
+              onPress={() => navigation.goBack()}
+            >
+              <Icon name="close" size={24} color="#000" />
+            </TouchableOpacity>
             <Image
-              source={require("../pictures/line.png")}
-              style={styles.line}
+              source={require("../../../../assets/logoWhite.png")}
+              style={styles.logo}
               resizeMode="contain"
             />
-          </SafeAreaView>
-          {/* <View style={styles.header}>
+          </View>
+          <Image
+            source={require("../pictures/line.png")}
+            style={styles.line}
+            resizeMode="contain"
+          />
+        </SafeAreaView>
+        {/* <View style={styles.header}>
             <Text style={styles.headText}>Package</Text>
             <TouchableOpacity
               onPress={() => setShowSearch(true)}
@@ -99,59 +87,86 @@ const CustomizePackage = () => {
               onSearch={handleSearch}
             />
           )} */}
-          <ScrollView>
-            {/* <Text style={styles.debutText}>Debut Packages</Text> */}
-           
-            <ScrollView 
-            horizontal 
-            showsHorizontalScrollIndicator={false} 
-            contentContainerStyle={styles.tabScrollViewContent} 
+        <ScrollView>
+          {/* <Text style={styles.debutText}>Debut Packages</Text> */}
+
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={styles.tabScrollViewContent}
           >
             <View style={styles.tabContainer}>
               {Object.keys(serviceData).map((tab) => (
                 <TouchableOpacity
                   key={tab}
-                  style={[styles.tabButton, selectedTab === tab && styles.activeTab]}
+                  style={[
+                    styles.tabButton,
+                    selectedTab === tab && styles.activeTab,
+                  ]}
                   onPress={() => setSelectedTab(tab)}
                 >
-                  <Text style={[styles.tabButtonText, selectedTab === tab && styles.activeTabText]}>{tab}</Text>
+                  <Text
+                    style={[
+                      styles.tabButtonText,
+                      selectedTab === tab && styles.activeTabText,
+                    ]}
+                  >
+                    {tab}
+                  </Text>
                 </TouchableOpacity>
               ))}
             </View>
           </ScrollView>
 
-          <ScrollView horizontal contentContainerStyle={styles.scrollViewContent}>
+          <ScrollView
+            horizontal
+            contentContainerStyle={styles.scrollViewContent}
+          >
             {serviceData[selectedTab].map((service, index) => (
-              <View key={index} style={styles.serviceButton} onPress={() => toggleService(service)}>
+              <View
+                key={index}
+                style={styles.serviceButton}
+                onPress={() => toggleService(service)}
+              >
                 <Image source={service.image} style={styles.serviceImage} />
                 <Text style={styles.serviceName}>{service.name}</Text>
-                <TouchableOpacity style={styles.addButtonContainer} onPress={() => toggleService(service)}>
-                  <Text style={styles.addButton}>{selectedServices.some((item) => item.name === service.name) ? '✔️' : '+'}</Text>
+                <TouchableOpacity
+                  style={styles.addButtonContainer}
+                  onPress={() => toggleService(service)}
+                >
+                  <Text style={styles.addButton}>
+                    {selectedServices.some((item) => item.name === service.name)
+                      ? "✔️"
+                      : "+"}
+                  </Text>
                 </TouchableOpacity>
                 <Text style={styles.servicePrice}>{service.price}k</Text>
               </View>
             ))}
           </ScrollView>
-            <View style={styles.summary}>
-              <Text style={styles.summaryTitle}>Name</Text>
-              <Text style={styles.summaryTitle}>Type</Text>
-              <Text style={styles.summaryTitle}>Price</Text>
-            </View>
-            <View style={styles.summaryContainer}>
-              {selectedServices.map((service, index) => (
-                <View key={index} style={styles.summaryRow}>
-                  <Text style={styles.summaryText}>{service.name}</Text>
-                  <Text style={styles.summaryText}>{service.type}</Text>
-                  <Text style={styles.summaryText}>{service.price}k</Text>
-                </View>
-              ))}
-              <Text style={styles.totalText}>TOTAL: {total}k</Text>
-            </View>
-            <TouchableOpacity style={styles.submitButton} onPress={handleSubmitPress}>
-              <Text style={styles.submitButtonText}>SAVE</Text>
-            </TouchableOpacity>
-          </ScrollView>
-        </View>
+          <View style={styles.summary}>
+            <Text style={styles.summaryTitle}>Name</Text>
+            <Text style={styles.summaryTitle}>Type</Text>
+            <Text style={styles.summaryTitle}>Price</Text>
+          </View>
+          <View style={styles.summaryContainer}>
+            {selectedServices.map((service, index) => (
+              <View key={index} style={styles.summaryRow}>
+                <Text style={styles.summaryText}>{service.name}</Text>
+                <Text style={styles.summaryText}>{service.type}</Text>
+                <Text style={styles.summaryText}>{service.price}k</Text>
+              </View>
+            ))}
+            <Text style={styles.totalText}>TOTAL: {total}k</Text>
+          </View>
+          <TouchableOpacity
+            style={styles.submitButton}
+            onPress={handleSubmitPress}
+          >
+            <Text style={styles.submitButtonText}>SAVE</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -162,7 +177,7 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
+    resizeMode: "cover",
   },
   head: {
     flexDirection: "row",
@@ -171,7 +186,7 @@ const styles = StyleSheet.create({
   closeButton: {
     padding: 10,
     marginTop: 5,
-    marginLeft: 8
+    marginLeft: 8,
   },
   logo: {
     flex: 1,
@@ -181,48 +196,48 @@ const styles = StyleSheet.create({
   iconButton: {
     marginLeft: -20,
     padding: 10,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 25,
     width: 40,
     height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   line: {
     marginLeft: "auto",
     marginRight: "auto",
     alignItems: "center",
-    marginTop: 5
+    marginTop: 5,
   },
   line2: {
     marginLeft: "auto",
     marginRight: "auto",
     alignItems: "center",
     marginTop: 15,
-    marginBottom: 10
+    marginBottom: 10,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     padding: 10,
   },
   headText: {
-    color: '#e6b800',
+    color: "#e6b800",
     fontSize: 22,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontFamily: "Poppins",
   },
   headerText: {
-    color: '#e6b800',
+    color: "#e6b800",
     fontSize: 14,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     textAlign: "center",
     marginTop: 10,
     fontFamily: "Poppins",
   },
   shText: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
     textAlign: "left",
     marginStart: 20,
@@ -230,34 +245,34 @@ const styles = StyleSheet.create({
     fontFamily: "Poppins",
   },
   searchButton: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     borderRadius: 15,
     padding: 5,
   },
   searchButtonText: {
-    color: '#000',
+    color: "#000",
     fontSize: 18,
     fontFamily: "Poppins",
   },
   bannerImage: {
-    width: '100%',
+    width: "100%",
     height: 150,
-    resizeMode: 'contain',
+    resizeMode: "contain",
     marginTop: 10,
   },
   debutText: {
-    color: '#000',
+    color: "#000",
     fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginVertical: 10,
     fontFamily: "Poppins",
   },
   customizeText: {
-    color: '#000',
+    color: "#000",
     fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
     marginBottom: 10,
     fontFamily: "Poppins",
   },
@@ -265,12 +280,12 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
   },
   tabContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
     marginVertical: 10,
   },
   headerEType: {
-    color: '#000',
+    color: "#000",
     fontSize: 14,
     marginBottom: -2,
     marginTop: 20,
@@ -282,23 +297,23 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
     borderRadius: 20,
     borderWidth: 1,
-    borderColor: '#000',
+    borderColor: "#000",
     marginHorizontal: 3,
     marginTop: 20,
   },
   tabButtonText: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
     fontFamily: "Poppins",
   },
   activeTab: {
-    backgroundColor: '#e6b800',
+    backgroundColor: "#e6b800",
     borderWidth: 1,
-    borderColor: '#e6b800',
+    borderColor: "#e6b800",
     elevation: 5,
   },
   activeTabText: {
-    color: '#1e1e1e',
+    color: "#1e1e1e",
     fontFamily: "Poppins",
   },
   scrollViewContent: {
@@ -306,7 +321,7 @@ const styles = StyleSheet.create({
     paddingBottom: 20,
   },
   serviceButton: {
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 10,
     marginBottom: 15,
     padding: 10,
@@ -315,91 +330,91 @@ const styles = StyleSheet.create({
   serviceImage: {
     width: 300,
     height: 200,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 10,
   },
   serviceName: {
-    color: '#000',
+    color: "#000",
     fontSize: 18,
     marginTop: 10,
     textAlign: "left",
     fontFamily: "Poppins",
   },
   servicePrice: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
     marginTop: 5,
     textAlign: "left",
     fontFamily: "Poppins",
   },
   addButtonContainer: {
-    backgroundColor: '#000',
+    backgroundColor: "#000",
     borderRadius: 50,
     width: 30,
     height: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginTop: 10,
   },
   addButton: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    textAlign: 'center',
+    textAlign: "center",
     fontFamily: "Poppins",
   },
   summary: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginHorizontal: 20,
     marginBottom: 5,
   },
   summaryTitle: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
-    fontWeight: 'bold',
-    width: '33%',
-    textAlign: 'center',
+    fontWeight: "bold",
+    width: "33%",
+    textAlign: "center",
     fontFamily: "Poppins",
   },
   summaryContainer: {
     padding: 10,
-    backgroundColor: '#f5f5f5',
+    backgroundColor: "#f5f5f5",
     borderRadius: 10,
     margin: 10,
   },
   summaryRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginBottom: 5,
   },
   summaryText: {
-    color: '#000',
+    color: "#000",
     fontSize: 14,
-    width: '33%',
-    textAlign: 'center',
+    width: "33%",
+    textAlign: "center",
     fontFamily: "Poppins",
   },
   totalText: {
-    color: '#000',
+    color: "#000",
     fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'right',
+    fontWeight: "bold",
+    textAlign: "right",
     marginTop: 10,
     fontFamily: "Poppins",
   },
   submitButton: {
-    backgroundColor: '#FFC42B',
+    backgroundColor: "#FFC42B",
     padding: 15,
     borderRadius: 25,
     marginLeft: 100,
     marginRight: 100,
     margin: 20,
-    alignItems: 'center',
+    alignItems: "center",
   },
   submitButtonText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 18,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     fontFamily: "Poppins",
   },
 });
